80503161432ba79c09718e24a3ad823f30bc922e
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2023-01-26 00:39:49 -0500

    Update finance.py

--- finance.py
+++ finance.py
@@ -4,41 +4,56 @@ import matplotlib.pyplot as plt
 from unicodedata import normalize
 from urllib.request import Request, urlopen
 
-y2021 = {}
-y2020 = {}
-y2019 = {}
-y2018 = {}
-y2017 = {}
-businessvalue = {}
+#----------------------------------------------
+#Data-Structures
+#Constants
+million = 1000000
 
-EBITDA = [0,0,0,0,0]
-FCF = [0,0,0,0,0]
+#EBITDA Web-Scraped Values
+EBITDA = {}
+EBITDA_YOY = {}
 
-# tickername = input()
-tickername = 'tsla'
-
-
-# driver = webdriver.Chrome(DRIVER_PATH)
-# driver.get("https://stockanalysis.com/stocks/{}/".format(tickername))
-# time.sleep(3)
-# driver.get("https://stockanalysis.com/stocks/{}/financials/".format(tickername))
-
-
-req = Request(url='https://stockanalysis.com/stocks/{}/financials/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-
-table_MN = pd.read_html(webpage)
-#print(f'Total tables: {len(table_MN)}')
-
-
-table_MN = pd.read_html(webpage, match='Revenue')
-
-print(table_MN[0]['2021'][0:9])   #EBITDA (NEED)
-                                    #->YOY Change, AVG EBITA GROWTH
-                                  #Past Free Cash Flow (NEED)
-                                    #->YOY Change, AVG Past Free Cashflow
-                                  #NOT ON THE WEBSITE
-                                    #Assumptions {EBITDA growth rate, free cash flow rate, rate of return, terminal growth rate, ev/evita multiple }
+#FCF Web-Scraped Values
+FCF = {}
+#----------------------------------------------
 
 
 
+#tickername = input().lower()    #USED TO OBTAIN TICKER TAG NAME
+tickername = 'coke'   #Comment this when we do live implementation and uncomment above
+print("Provide the year to start the DCF:")   
+year = input() #Allows for DCF model to render for whichever year
+years = [year,str(int(year)-1),str(int(year)-2),str(int(year)-3),str(int(year)-4)]
+
+
+#Financial Information
+financialreq = Request(url='https://stockanalysis.com/stocks/{}/financials/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
+financials = urlopen(financialreq).read()
+table_finance = pd.read_html(financials)
+#print(table_finance[0]['2022'][0:])   
+
+cashflowreq = Request(url='https://stockanalysis.com/stocks/{}/financials/cash-flow-statement/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
+cashflows = urlopen(cashflowreq).read()
+table_cashflow = pd.read_html(cashflows)
+#print(table_cashflow[0]['2022'][0:])
+
+
+
+# for each in years:
+#   EBITDA[each] = table_finance[0][each][28]
+#   FCF[each] = table_cashflow[0][each][17]
+
+
+
+# print(EBITDA)
+# print(FCF)
+
+
+
+
+  #EBITDA (NEED)
+    #->YOY Change, AVG EBITA GROWTH
+  #Past Free Cash Flow (NEED)
+    #->YOY Change, AVG Past Free Cashflow
+  #NOT ON THE WEBSITE
+    #Assumptions {EBITDA growth rate, free cash flow rate, rate of return, terminal growth rate, ev/evita multiple }
