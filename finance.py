8ccb8f241181e00c688575848f7c5fa5d400e6cd
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2023-01-26 01:32:32 -0500

    Update finance.py

--- finance.py
+++ finance.py
@@ -20,9 +20,11 @@ FCF = {}
 
 
 #tickername = input().lower()    #USED TO OBTAIN TICKER TAG NAME
-tickername = 'coke'   #Comment this when we do live implementation and uncomment above
-print("Provide the year to start the DCF:")   
-year = input() #Allows for DCF model to render for whichever year
+tickername = 'aapl'   #Comment this when we do live implementation and uncomment above
+
+#print("Provide the year to start the DCF:")   
+#year = input() #Allows for DCF model to render for whichever year
+year = "2022"
 years = [year,str(int(year)-1),str(int(year)-2),str(int(year)-3),str(int(year)-4)]
 
 
@@ -30,7 +32,9 @@ years = [year,str(int(year)-1),str(int(year)-2),str(int(year)-3),str(int(year)-4
 financialreq = Request(url='https://stockanalysis.com/stocks/{}/financials/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
 financials = urlopen(financialreq).read()
 table_finance = pd.read_html(financials)
-#print(table_finance[0]['2022'][0:])   
+t_finance = table_finance[0]
+print(t_finance)
+t_finance.columns = years
 
 cashflowreq = Request(url='https://stockanalysis.com/stocks/{}/financials/cash-flow-statement/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
 cashflows = urlopen(cashflowreq).read()
