01151c150dcdac58c2e91eca0d1449423d565d10
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2023-01-29 21:57:27 -0500

    Restructured Tables to allow for searching rows as well instead of just columns
    -Added functionality to search using rows
    -Have to add try and catch incase the year you provide doesn't have data provided EX) You request 2022-2017 data but the 2022 isn't updated. Hence, we will get an error.
    
    -Next steps: Begin to make a class structure and functions to map instead of running for loops. Improve code optimization.

--- finance.py
+++ finance.py
@@ -4,60 +4,38 @@ import matplotlib.pyplot as plt
 from unicodedata import normalize
 from urllib.request import Request, urlopen
 
-#----------------------------------------------
-#Data-Structures
-#Constants
-million = 1000000
+EBITDA = []
+FCF = []
+years = []
+# tickername = input()
+tickername = 'AAPL'
 
-#EBITDA Web-Scraped Values
-EBITDA = {}
-EBITDA_YOY = {}
+year = '2021'
+years = [str(int(year)),str(int(year)-1),str(int(year)-2),str(int(year)-3), str(int(year)-4)]
+print(years)
 
-#FCF Web-Scraped Values
-FCF = {}
-#----------------------------------------------
+req = Request(url='https://stockanalysis.com/stocks/{}/financials/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
+webpage = urlopen(req).read()
+table_MN = pd.read_html(webpage, index_col = 0)
+df = table_MN[0]
 
+req2 = Request(url='https://stockanalysis.com/stocks/{}/financials/cash-flow-statement/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
+webpage1 = urlopen(req2).read()
+table_MN2 = pd.read_html(webpage1, index_col=0)
+df2 = table_MN2[0]
 
+for each in years:
+  EBITDA.append(df[each]['EBITDA'])
+  FCF.append(df2[each]['Free Cash Flow'])
+print(EBITDA)  
+print(FCF)
 
-#tickername = input().lower()    #USED TO OBTAIN TICKER TAG NAME
-tickername = 'aapl'   #Comment this when we do live implementation and uncomment above
-
-#print("Provide the year to start the DCF:")   
-#year = input() #Allows for DCF model to render for whichever year
-year = "2022"
-years = [year,str(int(year)-1),str(int(year)-2),str(int(year)-3),str(int(year)-4)]
-
-
-#Financial Information
-financialreq = Request(url='https://stockanalysis.com/stocks/{}/financials/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
-financials = urlopen(financialreq).read()
-table_finance = pd.read_html(financials)
-t_finance = table_finance[0]
-print(t_finance)
-t_finance.columns = years
-
-cashflowreq = Request(url='https://stockanalysis.com/stocks/{}/financials/cash-flow-statement/'.format(tickername), headers={'User-Agent' : 'Mozilla/5.0'})
-cashflows = urlopen(cashflowreq).read()
-table_cashflow = pd.read_html(cashflows)
-#print(table_cashflow[0]['2022'][0:])
+#EBITDA (NEED)              
+# #->YOY Change, AVG EBITA GROWTH
+#Past Free Cash Flow (NEED)
+#->YOY Change, AVG Past Free Cashflow
+#NOT ON THE WEBSITE
+#Assumptions {EBITDA growth rate, free cash flow rate, rate of return, terminal growth rate, ev/evita multiple }
 
 
 
-# for each in years:
-#   EBITDA[each] = table_finance[0][each][28]
-#   FCF[each] = table_cashflow[0][each][17]
-
-
-
-# print(EBITDA)
-# print(FCF)
-
-
-
-
-  #EBITDA (NEED)
-    #->YOY Change, AVG EBITA GROWTH
-  #Past Free Cash Flow (NEED)
-    #->YOY Change, AVG Past Free Cashflow
-  #NOT ON THE WEBSITE
-    #Assumptions {EBITDA growth rate, free cash flow rate, rate of return, terminal growth rate, ev/evita multiple }
