1b8db9d79bb621bf7d6fa71084749771b6855428
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2023-02-01 17:01:10 -0500

    Update finance.py

--- finance.py
+++ finance.py
@@ -8,20 +8,13 @@ class DCF:
   #Obtained from user (Assumptions):
   growthrate_ebitda = 0.05   #Fixed for now, this will change to variable once we implement base DCF
   growthrate_fcf = 0.10
-  rateofreturn = 0.125   #Used for discount array
+   #Used for discount array
   discount = []
-
-  
-
-  
-
 
   #Obtained from web:
   EBITDA = []
   FCF = []
   years = []
-
-
 
   def __init__(self,ticker,year):
     self.ticker = ticker
@@ -31,33 +24,35 @@ class DCF:
     
     self.financetable = pd.read_html(urlopen(self.reqFinance).read(), index_col = 0)[0]
     self.cashflowtable = pd.read_html(urlopen(self.reqCashflow).read(), index_col = 0)[0]
+
     self.years = [str(int(year)),str(int(year)-1),str(int(year)-2),str(int(year)-3), str(int(year)-4)]
 
-    for each in self.years:
-      self.setupEBITDA_FCF(each)
-    
-  
+    try:
+      for each in self.years:
+        self.setupEBITDA_FCF(each)
+    except:
+      self.EBITDA = []
+      self.FCF = []
+      self.setupEBITDA_FCF(self.year)
+    finally:
+      if len(self.EBITDA) == 1:
+        print("Could only print current year cause values are missing")
+        print("EBITDA: {}".format(self.EBITDA))
+        print("FCF: {}".format(self.FCF))
+      else:
+        print("EBITDA: {}".format(self.EBITDA))
+        print("FCF: {}".format(self.FCF))
+        
   def setupEBITDA_FCF(self,year):
     self.EBITDA.append(self.financetable[year]['EBITDA'])
     self.FCF.append(self.cashflowtable[year]['Free Cash Flow'])
+    
   
   def setupDiscount(self,rateofreturn):
     rateofreturn /= 100
     for x in range(1,6):
       sol = (1/((1+rateofreturn)**x))
       self.discount.append(sol)
-
-  #Request 
-  # def func_finance(self):
-  #   webpage = urlopen(self.reqFinance).read()
-  #   table_MN = pd.read_html(webpage, index_col = 0)
-  #   return table_MN[0]
-
-  # def func_cashflow(self):
-  #   webpage = urlopen(self.reqCashflow).read()
-  #   table_MN = pd.read_html(webpage, index_col = 0)
-  #   return table_MN[0]
-
 
 #Main CLASS
 print("Enter Ticker-Name: ")
@@ -67,17 +62,10 @@ print("Enter Year: ")
 year = input()
 
 cfm = DCF(ticker,year)
-print(cfm.EBITDA)  
-print(cfm.FCF)
-cfm.setupDiscount(12.5)
+cfm.setupDiscount(15.5)
 
 print(cfm.discount)
-#EBITDA (NEED)              
-# #->YOY Change, AVG EBITA GROWTH
-#Past Free Cash Flow (NEED)
-#->YOY Change, AVG Past Free Cashflow
-#NOT ON THE WEBSITE
-#Assumptions {EBITDA growth rate, free cash flow rate, rate of return, terminal growth rate, ev/evita multiple }
+
 
 
 
