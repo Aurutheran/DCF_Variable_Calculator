b61efaa72687c662c5f0cc3bf6084471bb294c76
77ruthvik <74626667+77ruthvik@users.noreply.github.com>
2023-02-14 16:33:32 -0500

    Connected the Frontend with Backend

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
+#FastAPI Code
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+#Finance Code
+import finance
+
 app = FastAPI()
 res = {}
+DCFValue = 0
+CurrentValue = 0
+MarginOfSafety = 0
 
 app.mount("/static", StaticFiles(directory="static"), name="static") #html_css
 
-templates = Jinja2Templates(directory="templates") #html_css
+templates = Jinja2Templates(directory="html_css") #html_css
 
 @app.get('/')
 def render(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request})
+    #return templates.TemplateResponse("item.html", {"request": request, "DCFValue": 0, "CurrentValue": 0, "MarginOfSafety": 0})
+    return templates.TemplateResponse("item.html", {"request": request, "DCFValue": DCFValue, "CurrentValue": CurrentValue, "MarginOfSafety": MarginOfSafety})
 
 @app.post('/')
 async def run(request: Request):
     data = await request.form()
     res.update(data)
     print(data)
-    return templates.TemplateResponse("item.html", {"request": request, "data": data})
+    ticker = res['ticker']
+    year = res['year']
+    ror = res['ROR']
+    ev_ebidta = res['EV/EBIDTA']
+    Terminal_ROR = res['TerminalROR']
+    ebidta_gr = res['EBIDTA_GR']
+    fcf = res['FCF']
+
+    cfm = finance.DCF(ticker, year)
+    cfm.setupDiscount(ror)
+    cfm.setEV_EBITDA(ev_ebidta)
+    cfm.setupTerminal(Terminal_ROR)
+    cfm.setupEBITDA_GR(ebidta_gr)
+    cfm.setupFCF_GR(fcf)
+
+    cfm.obtainGrowthMethod()
+    cfm.setGrowthAverage()
+    cfm.obtainSharePrice()
+
+    DCFValue = round(cfm.Share_Price,2)
+
+    
+    return templates.TemplateResponse("item.html", {"request": request, "data": data, "DCFValue": DCFValue, "CurrentValue": CurrentValue, "MarginOfSafety": MarginOfSafety})
 
 @app.get('/getval')
 def val():
